b30a93b3a4b0ab8a6bc2911acaa69e34d7f966bb
John Foley <jfoley@cs.umass.edu>
2015-03-13 17:14:56 -0400

    working on stuff

---
+++ src/main/java/jfoley/vocabpress/TFDF.java
@@ -0,0 +1,50 @@
+package jfoley.vocabpress;
+
+import jfoley.vocabpress.io.Coder;
+import jfoley.vocabpress.io.coders.VByteCoders;
+import jfoley.vocabpress.io.util.BufferList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * @author jfoley
+ */
+public class TFDF {
+  public int tf;
+  public int df;
+  public TFDF() {
+    this(0,0);
+  }
+  public TFDF(int tf, int df) {
+    this.tf = tf;
+    this.df = df;
+  }
+  public void add(TFDF other) {
+    this.tf += other.tf;
+    this.df += other.df;
+  }
+
+  public static Coder<TFDF> coder = new Coder<TFDF>() {
+    @Override
+    public boolean knowsOwnSize() {
+      return true;
+    }
+
+    @Override
+    public ByteBuffer writeImpl(TFDF obj) throws IOException {
+      BufferList bl = new BufferList();
+      bl.add(VByteCoders.ints.write(obj.tf));
+      bl.add(VByteCoders.ints.write(obj.df));
+      return bl.compact();
+    }
+
+    @Override
+    public TFDF readImpl(InputStream inputStream) throws IOException {
+      int tf = VByteCoders.ints.read(inputStream);
+      int df = VByteCoders.ints.read(inputStream);
+      return new TFDF(tf, df);
+    }
+  };
+}
