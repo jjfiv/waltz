5d6415bd73ce443e6f35a1ba48d2560bfe96966c
John Foley <jfoley@cs.umass.edu>
2015-06-05 09:00:58 -0400

    Add nullability annotations to Index.java

--- base/src/main/java/edu/umass/cs/ciir/waltz/index/Index.java
+++ base/src/main/java/edu/umass/cs/ciir/waltz/index/Index.java
@@ -4,6 +4,8 @@ import edu.umass.cs.ciir.waltz.dociter.movement.PostingMover;
 import edu.umass.cs.ciir.waltz.feature.Feature;
 import edu.umass.cs.ciir.waltz.postings.positions.PositionsList;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.util.List;
 
@@ -16,19 +18,32 @@ public interface Index extends Closeable {
 
   int getDocumentCount();
 
+  @Nonnull
   List<Integer> getAllDocumentIds();
 
+  @Nullable
   PostingMover<Integer> getCountsMover(String term);
 
+  @Nullable
   PostingMover<PositionsList> getPositionsMover(String term);
 
+  @Nullable
   Feature<Integer> getCounts(String term);
 
+  @Nullable
   Feature<PositionsList> getPositions(String term);
 
+  @Nullable
   String getDocumentName(int id);
+
+  /**
+   * Returns -1 when not found.
+   * @param documentName the document name to query.
+   * @return the identifier or -1 if not found.
+   */
   int getDocumentId(String documentName);
 
+  @Nonnull
   Feature<Integer> getLengths();
 
 }
