3c738dff2a4ac58f2f1a8074e6752d0f8ae82fda
John Foley <jfoley@cs.umass.edu>
2015-06-10 17:20:21 -0400

    Create IdMaps from coop subproject

---
+++ base/src/main/java/edu/umass/cs/ciir/waltz/IdMaps.java
@@ -0,0 +1,58 @@
+package edu.umass.cs.ciir.waltz;
+
+import edu.umass.cs.ciir.waltz.coders.map.IOMap;
+import edu.umass.cs.ciir.waltz.coders.map.IOMapWriter;
+
+import java.io.Closeable;
+import java.io.Flushable;
+import java.io.IOException;
+
+/**
+ * @author jfoley.
+ */
+public class IdMaps {
+  public static class Writer<V extends Comparable<V>> implements Flushable, Closeable {
+    public final IOMapWriter<Integer, V> forwardWriter;
+    public final IOMapWriter<V, Integer> reverseWriter;
+
+    public Writer(IOMapWriter<Integer, V> forwardWriter, IOMapWriter<V, Integer> reverseWriter) throws IOException {
+      this.forwardWriter = forwardWriter.getSorting();
+      this.reverseWriter = reverseWriter.getSorting();
+    }
+
+    public void put(int id, V value) throws IOException {
+      forwardWriter.put(id, value);
+      reverseWriter.put(value, id);
+    }
+
+    @Override
+    public void close() throws IOException {
+      forwardWriter.close();
+      reverseWriter.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+      forwardWriter.flush();
+      reverseWriter.flush();
+    }
+
+  }
+
+  public static class Reader<V extends Comparable<V>> implements Closeable {
+    public final IOMap<Integer, V> forwardReader;
+    public final IOMap<V, Integer> reverseReader;
+
+    public Reader(IOMap<Integer, V> forwardReader, IOMap<V, Integer> reverseReader) {
+      this.forwardReader = forwardReader;
+      this.reverseReader = reverseReader;
+    }
+
+    @Override
+    public void close() throws IOException {
+      this.forwardReader.close();
+      this.reverseReader.close();
+    }
+  }
+
+}
--- galago-io/src/main/java/edu/umass/cs/ciir/waltz/galago/io/GalagoIO.java
+++ galago-io/src/main/java/edu/umass/cs/ciir/waltz/galago/io/GalagoIO.java
@@ -1,5 +1,6 @@
 package edu.umass.cs.ciir.waltz.galago.io;
 
+import edu.umass.cs.ciir.waltz.IdMaps;
 import edu.umass.cs.ciir.waltz.coders.Coder;
 import edu.umass.cs.ciir.waltz.coders.map.*;
 import org.lemurproject.galago.utility.Parameters;
@@ -32,5 +33,19 @@ public class GalagoIO {
 
   public static RawIOMapWriter getRawIOMapWriter(String path) throws IOException {
     return new RawGalagoDiskMapWriter(path);
+  }
+
+  public static <V extends Comparable<V>> IdMaps.Writer<V> openIdMapsWriter(String baseName, Coder<Integer> keyCoder, Coder<V> valCoder) throws IOException {
+    return new IdMaps.Writer<>(
+        getIOMapWriter(keyCoder, valCoder, baseName + ".fwd"),
+        getIOMapWriter(valCoder, keyCoder, baseName+".rev")
+    );
+  }
+
+  public static <V extends Comparable<V>> IdMaps.Reader<V> openIdMapsReader(String baseName, Coder<Integer> keyCoder, Coder<V> valCoder) throws IOException {
+    return new IdMaps.Reader<>(
+        openIOMap(keyCoder, valCoder, baseName + ".fwd"),
+        openIOMap(valCoder, keyCoder, baseName+".rev")
+    );
   }
 }
