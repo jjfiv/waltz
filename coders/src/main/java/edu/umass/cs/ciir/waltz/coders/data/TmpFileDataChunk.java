ec6b9f39b88f523e315c8859111467021e2360f1
John Foley <jfoley@cs.umass.edu>
2015-06-12 17:07:25 -0400

    Depend on TemporaryFile being smart instead

--- coders/src/main/java/edu/umass/cs/ciir/waltz/coders/data/TmpFileDataChunk.java
+++ coders/src/main/java/edu/umass/cs/ciir/waltz/coders/data/TmpFileDataChunk.java
@@ -2,10 +2,10 @@ package edu.umass.cs.ciir.waltz.coders.data;
 
 import ciir.jfoley.chai.IntMath;
 import ciir.jfoley.chai.io.StreamFns;
+import ciir.jfoley.chai.io.TemporaryFile;
 import ciir.jfoley.chai.lang.annotations.EmergencyUseOnly;
 import edu.umass.cs.ciir.waltz.coders.Coder;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,8 +13,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static java.nio.file.StandardOpenOption.*;
 
@@ -26,15 +24,13 @@ import static java.nio.file.StandardOpenOption.*;
  */
 @EmergencyUseOnly
 class TmpFileDataChunk implements MutableDataChunk {
-  private final File tmp;
+  private final TemporaryFile tmp;
   private final FileChannel channel;
   private long size;
-  private boolean closed = false;
-  private Logger logger = Logger.getLogger(TmpFileDataChunk.class.getName());
 
   public TmpFileDataChunk() throws IOException {
-    this.tmp = File.createTempFile("tfdc", "datachunk");
-    this.channel = FileChannel.open(tmp.toPath(), CREATE, WRITE, READ);
+    this.tmp = new TemporaryFile("tfdc", "datachunk");
+    this.channel = FileChannel.open(tmp.get().toPath(), CREATE, WRITE, READ);
     this.size = 0L;
   }
   @Override
@@ -108,17 +104,8 @@ class TmpFileDataChunk implements MutableDataChunk {
   }
 
   @Override
-  public void finalize() throws Throwable {
-    super.finalize();
-    if(!closed) logger.log(Level.SEVERE, "Leaked TmpFileDataChunk!");
-    assert(closed);
-  }
-
-  @Override
   public void close() throws IOException {
-    closed = true;
     channel.close();
-    boolean status = tmp.delete();
-    assert(status) : "Couldn't delete temporary file!";
+    tmp.close();
   }
 }
