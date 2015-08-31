06b157546e04b8ad952d4d78f9a5d93a2165d74f
John Foley <jfoley@cs.umass.edu>
2015-08-31 16:46:49 -0400

    document metadata class

--- base/src/main/java/edu/umass/cs/ciir/waltz/sys/KeyMetadata.java
+++ base/src/main/java/edu/umass/cs/ciir/waltz/sys/KeyMetadata.java
@@ -4,11 +4,27 @@ package edu.umass.cs.ciir.waltz.sys;
  * @author jfoley
  */
 public interface KeyMetadata<V, Subclass extends KeyMetadata<V, ?>> {
+  /**
+   * Any type of metadata must at least know how many documents were seen.
+   * @return the total number of documents.
+   */
   int totalDocuments();
 
+  /**
+   * Add in the information as represented by the other metadata object.
+   * @param m another metadata object of the same class.
+   */
   void accumulate(Subclass m);
 
+  /**
+   * visit a document with a given value:
+   * @param document the document id
+   * @param item the value
+   */
   void accumulate(int document, V item);
 
+  /**
+   * @return an instance of this metadata which is semantically equivalent to zero.
+   */
   Subclass zero();
 }
